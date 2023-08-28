f454227fb01f41090d35f20a121364761108532e
MrSpex <mail@dannilsson.se>
2023-08-28 20:42:53 +0200

    feat: add fluent gql

---
+++ src/commands/helpers/graphql/gql-client.ts
@@ -0,0 +1,55 @@
+import { FluentGql, FluentGqlOptions } from "./types";
+
+async function gqlFetch(options: FluentGqlOptions): Promise<any> {
+  const response = await (options.fetch || fetch)(options.url, {
+    ...options.init,
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      ...options.headers,
+    },
+    body: JSON.stringify({
+      query: options.query,
+      variables: options.variables,
+    }),
+  });
+  return await response.json();
+}
+
+async function gqlFetchMap<T>(
+  options: FluentGqlOptions,
+  property: string,
+): Promise<T> {
+  const { data, errors } = await gqlFetch(options);
+  const [error] = [...(errors || [])].map((error_1) =>
+    Object.assign(new Error(error_1.message), error_1),
+  );
+  if (error) {
+    throw error;
+  }
+  return data[property] as T;
+}
+
+export function gqlClient(
+  options: FluentGqlOptions = {
+    init: null,
+    url: "/api/v1/haffa/graphql",
+    headers: {},
+    query: "",
+    variables: null,
+  },
+): FluentGql {
+  return {
+    init: (init) => gqlClient({ ...options, init }),
+    fetch: (fetch) => gqlClient({ ...options, fetch }),
+    url: (url) => gqlClient({ ...options, url }),
+    headers: (headers) => gqlClient({ ...options, headers }),
+    query: (query) => gqlClient({ ...options, query }),
+    variables: (variables) => gqlClient({ ...options, variables }),
+    map: <T>(property: string, fixup?: (value: T) => T) =>
+      gqlFetchMap<T>(options, property).then((value) =>
+        fixup ? fixup(value) : value,
+      ),
+  };
+}
---
+++ src/commands/helpers/graphql/types.ts
@@ -0,0 +1,18 @@
+export interface FluentGql {
+  init(init?: RequestInit): FluentGql;
+  fetch(f?: typeof fetch): FluentGql;
+  url(url: string): FluentGql;
+  headers(headers: Record<string, string>): FluentGql;
+  query(query: string): FluentGql;
+  variables(variables: any): FluentGql;
+  map<T>(property: string, fixup?: (value: T) => T): Promise<T>;
+}
+
+export interface FluentGqlOptions {
+  fetch?: typeof fetch;
+  init?: RequestInit | null | undefined;
+  url: string;
+  headers: Record<string, string>;
+  query: string;
+  variables: any | null;
+}
